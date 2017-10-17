cccbe2f59ab154d2b2f260fd503f558b4d72e2fd
hv <phamdangkhoa17@gmail.com>
2017-10-17 20:36:12 +0700

    Tong 2 so

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 const http = require('http');
+const url = require('url');
+const querystring = require('querystring');
 
-http.createServer(function(req, res) {
-    res.end('Xin chao cac ban');
-}).listen(3000, () => console.log('server is running'));
+const Server = http.createServer();
+
+Server.on('request', (req, res) => {
+    var duong_dan = req.url;
+
+    var duong_dan_parse = url.parse(duong_dan);
+
+    var param = querystring.parse(duong_dan_parse.query)
+    
+    var tong = +param.bien1 + +param.bien2;
+
+    res.end(tong + '');
+});
+
+Server.listen(3000, () => console.log('server is running'))
