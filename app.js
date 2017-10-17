e4d70c6e2bd0873283ea96d389861c49e2e41585
hv <phamdangkhoa17@gmail.com>
2017-10-17 20:43:28 +0700

    add some cmt

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const querystring = require('querystring');
 const Server = http.createServer();
 
 Server.on('request', (req, res) => {
+    // lay duong dan url tren browser
     var duong_dan = req.url;
 
+    // tach duong dan tren browser ra duong dan bien1, bien2
     var duong_dan_parse = url.parse(duong_dan);
 
+    // tach tung bien ra trong chuoi bien1,bien2
     var param = querystring.parse(duong_dan_parse.query)
     
     var tong = +param.bien1 + +param.bien2;
